4e0cd60193b18eb34a4eac0d50e8c0690b31dadd
Ian Rodney <ian.rodney@gmail.com>
2023-04-03 15:43:28 -0700

    fix up tests

--- tests/test_ray_secret_proxy.py
+++ tests/test_ray_secret_proxy.py
@@ -1,39 +1,37 @@
 from collections import defaultdict
 from typing import Any, Dict, List
-from aws_secret_operator import AWSRaySecretOperator
-from gcp_secret_operator import GCPRaySecretOperator
 
 import pytest
 import ray
 
-from ray_secret_operator import RaySecretOperator
-from ray_secret_proxy import RaySecretProxy
+from raysecretsproxy import AWSRaySecretOperator, GCPRaySecretOperator, RaySecretOperator, RaySecretProxy
 
 
 @pytest.fixture(scope="session", autouse=True)
 def ray_init():
-    ray.init()
+    ray.init(local_mode=True)
 
-class LocalSecretOperator(RaySecretOperator):
-    def __init__(self, values: Dict[str, bytes]):
-        self.values = values
-        self.initialized = False
-        self.num_calls = defaultdict(int)
-
-    def initialize(self) -> None:
-        self.initialized = True
-
-    def _fetch(self, name: str, **kwargs):
-        assert self.initialized
-        self.num_calls[name] += 1
-        return self.values[name], self.num_calls
-
-    def list_secrets(self, filter=Any) -> List[str]:
-        """Lists available secret names (not values)."""
-        return list(self.values.keys())
 
 
 def test_proxy():
+    class LocalSecretOperator(RaySecretOperator):
+        def __init__(self, values: Dict[str, bytes]):
+            self.values = values
+            self.initialized = False
+            self.num_calls = defaultdict(int)
+
+        def initialize(self) -> None:
+            self.initialized = True
+
+        def _fetch(self, name: str, **kwargs):
+            assert self.initialized
+            self.num_calls[name] += 1
+            return self.values[name], self.num_calls
+
+        def list_secrets(self, filter=Any) -> List[str]:
+            """Lists available secret names (not values)."""
+            return list(self.values.keys())
+
     operator = LocalSecretOperator({
         "abc" : b"abc"
     })
@@ -49,15 +47,16 @@ def test_proxy():
     assert secret.metadata["abc"] == 2
     
 
+@pytest.mark.skip("run manually with credentials automatically discoverable")
 @pytest.mark.parametrize(
     "operator",
     [
-        pytest.param(GCPRaySecretOperator(), id="gcp"),
-        pytest.param(AWSRaySecretOperator(), id="aws")
+        pytest.param(GCPRaySecretOperator, id="gcp"),
+        pytest.param(AWSRaySecretOperator, id="aws")
     ]
 )
 def test_actual_operators(operator: RaySecretOperator):
-    proxy = RaySecretProxy.remote(operator)
+    proxy = RaySecretProxy.remote(operator())
     assert isinstance(ray.get(proxy.list_secrets.remote()), list)
     with pytest.raises(Exception):
         ray.get(proxy.get_secret.remote("not_a_real_secret"))
