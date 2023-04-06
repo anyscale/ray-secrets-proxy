20a5e9aae16a4ed448c26edd17bfe7028f9532e4
Ian Rodney <ian.rodney@gmail.com>
2023-04-06 10:55:28 -0700

    [AWS] Reduce unnecessary Error Handling on SecretsManager (#3)

--- aws_secret_operator.py
+++ aws_secret_operator.py
@@ -22,28 +22,10 @@ class AWSRaySecretOperator(RaySecretOperator):
             kwargs["SecretId"] = secret_name
             response = self.__client.get_secret_value(**kwargs)
         except ClientError as e:
-            if e.response["Error"]["Code"] == "DecryptionFailureException":
-                # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
-            elif e.response["Error"]["Code"] == "InternalServiceErrorException":
-                # An error occurred on the server side.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
-            elif e.response["Error"]["Code"] == "InvalidParameterException":
-                # You provided an invalid value for a parameter.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
-            elif e.response["Error"]["Code"] == "InvalidRequestException":
-                # You provided a parameter value that is not valid for the current state of the resource.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
-            elif e.response["Error"]["Code"] == "ResourceNotFoundException":
-                # We can't find the resource that you asked for.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
-            else:
-                raise e
+            if e.response["Error"]["Code"] in ("InvalidParameterException", "InvalidRequestException", "ResourceNotFoundException"):
+                raise ValueError(f"Cannot find secret {secret_name} associated arguments: {kwargs}")
+
+            raise e
 
         # Decrypts secret using the associated KMS key.
         # Depending on whether the secret is a string or binary, one of these fields will be populated.
